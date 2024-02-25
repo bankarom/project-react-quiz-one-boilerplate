1056262947f4a02f76841bad310184daa30a3c51
bankarom <146808698+bankarom@users.noreply.github.com>
2024-02-25 15:19:00 +0530

    Add files via upload

---
+++ HomeComponent.jsx
@@ -0,0 +1,28 @@
+import  { Component } from 'react'
+import App from '../App'
+
+export default class HomeComponent extends Component {
+  render() {
+    return (
+    <div className='main'>
+    
+    <div className='first'>
+        <div>
+            <h2 className='Quiz'>Quiz App</h2>
+        </div>
+
+        <div className='Play'>
+            <button className='play'>Play</button>
+        </div>
+
+    </div>
+        
+      
+    </div>
+
+      
+    )
+  }
+}
+
+
